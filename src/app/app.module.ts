ac5de813978345f617a38db84e7664b035290372
Arkhipov Vladislav <arkhipovvladislav@mail.ru>
2023-01-26 16:52:10 +0400

    Made the basic markup of the catalog page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,49 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CatalogComponent } from './pages/catalog/catalog.component';
+import { ListResultsComponent } from './components/list-results/list-results.component';
+import { PaginationComponent } from './components/pagination/pagination.component';
+import { HeaderComponent } from './components/header/header.component';
+import { SelectComponent } from './components/select/select.component';
+import { SearchComponent } from './components/search/search.component';
+import { FooterComponent } from './components/footer/footer.component';
+
+// Angular Material
+import { MatButtonModule } from '@angular/material/button';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSelectModule } from '@angular/material/select';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CatalogComponent,
+    ListResultsComponent,
+    PaginationComponent,
+    HeaderComponent,
+    SelectComponent,
+    SearchComponent,
+    FooterComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+
+    // Angular Material
+    MatButtonModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatFormFieldModule,
+    MatSelectModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
