8a2e2cc935788c87e52aeb8709a58ff9e0475993
Arkhipov Vladislav <arkhipovvladislav@mail.ru>
2023-01-31 14:45:57 +0400

    Small changes

--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -1,5 +1,4 @@
 import { Component, Input } from '@angular/core';
-import { MovieService } from 'src/app/services/movie.service';
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
