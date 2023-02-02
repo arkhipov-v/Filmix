0372a3a92bd93a5cc1e3ce11d0a5f770d1c4b949
Arkhipov Vladislav <arkhipovvladislav@mail.ru>
2023-02-02 08:51:16 +0400

    Added ui-page to routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { CatalogComponent } from './pages/catalog/catalog.component';
 import { FavoritesComponent } from './pages/favorites/favorites.component';
 import { MovieDetailComponent } from './pages/movie-detail/movie-detail.component';
 import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
+import { UiComponent } from './pages/ui/ui.component';
 
 const routes: Routes = [
   { path: 'catalog', component: CatalogComponent },
@@ -11,6 +12,7 @@ const routes: Routes = [
   { path: 'filmix', redirectTo: '/catalog', pathMatch: 'full' },
   { path: 'movies/:id', component: MovieDetailComponent },
   { path: 'favorites', component: FavoritesComponent },
+  { path: 'ui', component: UiComponent },
   { path: '**', component: PageNotFoundComponent },
 ];
 
